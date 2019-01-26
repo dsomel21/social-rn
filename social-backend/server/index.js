1639dd0532a76f6919f532d21c73d7eb501a28f2
Dilraj <dsomel21@gmail.com>
2019-01-26 09:41:05 -0500

    Got the server

---
+++ social-backend/server/index.js
@@ -0,0 +1,14 @@
+import express from 'express';
+
+const app = express();
+
+const PORT = process.env.PORT || 3000;
+
+app.listen(PORT, err => {
+  if (err){
+    console.log(err);
+  }
+  else {
+    console.log(`App listening on PORT: ${PORT}`)
+  }
+});
